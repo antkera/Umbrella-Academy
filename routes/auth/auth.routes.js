b34d04b5e00a0393b5e283a9d1fe8f96b85b85b7
antkera <128487289+antkera@users.noreply.github.com>
2023-11-10 19:44:22 +0100

    "merge"

--- routes/auth/auth.routes.js
+++ routes/auth/auth.routes.js
@@ -81,62 +81,6 @@ router.get("/register", (req, res, next) => {
 
 // POST "auth/regiter" => Envia el formulario a la base de datos y redirige a "/"
 
-router.post("/register" , async(req, res, next) => {
-    const {firstName, lastName, phone, age, email, password, secondPassword} = req.body;
-    if (password !== secondPassword){
-        res.status(400).render("auth/register", {firstName, lastName, phone, age, email,
-             erroMatchrPaswordMessage: "Both passwords must be the same."})
-        
-    return
-}
-    if (!firstName || !lastName || !email || !password) {
-        res.status(400).render("auth/register", {firstName, lastName, phone, age, email,
-             errorFullfillMessage: "The first three fields must be filled in"})
-return
-} 
-const passwordRegex =
-      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm;
-    if (!passwordRegex.test(password)) {
-      res.status(400).render("auth/register", {
-        firstName, lastName, phone, age, email, 
-        errorPassRegexMessage:
-          "Password is no secure enough. At least should have 8 caracters, one capital letter and one number",
-      });
-      return;
-    }
-    const emailRegex =
-    /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
-    if(!emailRegex.test(email)) {
-      res.status(400).render("auth/register", {
-        firstName, lastName, phone, age, email, 
-          errorMailRegexMessage:
-          "Email is not valid. Please enter a valid email address"
-      });
-      return;
-    }
-    
-    
-    
-    
-    
-    
-    // try {
-    //    await User.create({firstName, lastName, phone, age, email, password})
-       
-    // } catch (error) {
-    //     next(error)
-    // }
-
-    // console.log(req.body);
-    res.redirect("/")
-
-
-
-
-    
-        
-    
-})
 
 router.post("/register" , async(req, res, next) => {
     const {firstName, lastName, phone, age, email, password, secondPassword} = req.body;
