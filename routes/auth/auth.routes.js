d678931dcea6d030d459b17ec22f4439d46ec7c0
jairohrdev <j.gcarballa.dev@gmail.com>
2023-11-11 18:17:17 +0100

    Auth/layout

--- routes/auth/auth.routes.js
+++ routes/auth/auth.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const User = require("../../models/User.model");;
+const User = require("../../models/User.model");
 const capitalize = require("../../utils/capitalize");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
@@ -81,62 +81,81 @@ router.get("/register", (req, res, next) => {
 
 // POST "auth/regiter" => Envia el formulario a la base de datos y redirige a "/"
 
-
-router.post("/register" , async(req, res, next) => {
-    const {firstName, lastName, phone, age, email, password, secondPassword} = req.body;
-    if (password !== secondPassword){
-        res.status(400).render("auth/register", {firstName, lastName, phone, age, email,
-             erroMatchrPaswordMessage: "Both passwords must be the same."})
-        
-    return
-}
-    if (!firstName || !lastName || !email || !password) {
-        res.status(400).render("auth/register", {firstName, lastName, phone, age, email,
-             errorFullfillMessage: "The first three fields must be filled in"})
-return
-} 
-const passwordRegex =
-      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm;
-    if (!passwordRegex.test(password)) {
-      res.status(400).render("auth/register", {
-        firstName, lastName, phone, age, email, 
-        errorPassRegexMessage:
-          "Password is no secure enough. At least should have 8 caracters, one capital letter and one number",
+router.post("/register", async (req, res, next) => {
+  const { firstName, lastName, phone, age, email, password, secondPassword } =
+    req.body;
+  if (password !== secondPassword) {
+    res
+      .status(400)
+      .render("auth/register", {
+        firstName,
+        lastName,
+        phone,
+        age,
+        email,
+        erroMatchrPaswordMessage: "Both passwords must be the same.",
       });
-      return;
-    }
-    const emailRegex =
+
+    return;
+  }
+  if (!firstName || !lastName || !email || !password) {
+    res
+      .status(400)
+      .render("auth/register", {
+        firstName,
+        lastName,
+        phone,
+        age,
+        email,
+        errorFullfillMessage: "The first three fields must be filled in",
+      });
+    return;
+  }
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm;
+  if (!passwordRegex.test(password)) {
+    res.status(400).render("auth/register", {
+      firstName,
+      lastName,
+      phone,
+      age,
+      email,
+      errorPassRegexMessage:
+        "Password is no secure enough. At least should have 8 caracters, one capital letter and one number",
+    });
+    return;
+  }
+  const emailRegex =
     /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
-    if(!emailRegex.test(email)) {
-      res.status(400).render("auth/register", {
-        firstName, lastName, phone, age, email, 
-          errorMailRegexMessage:
-          "Email is not valid. Please enter a valid email address"
-      });
-      return;
-    }
-    
-    
-    
-    
-    
-    
-    // try {
-    //    await User.create({firstName, lastName, phone, age, email, password})
-       
-    // } catch (error) {
-    //     next(error)
-    // }
+  if (!emailRegex.test(email)) {
+    res.status(400).render("auth/register", {
+      firstName,
+      lastName,
+      phone,
+      age,
+      email,
+      errorMailRegexMessage:
+        "Email is not valid. Please enter a valid email address",
+    });
+    return;
+  }
 
-    // console.log(req.body);
-    res.redirect("/")
+  // try {
+  //    await User.create({firstName, lastName, phone, age, email, password})
 
+  // } catch (error) {
+  //     next(error)
+  // }
 
+  // console.log(req.body);
+  res.redirect("/");
+});
 
+// GET "/auth/logout" => cerrar la sesión activa del usuario y redireccionarlo a "/"
+router.get("/logout", (req, res, next) => {
+  req.session.destroy(() => {
+    // despues de destruir la sesion que quieres hacer?
+    res.redirect("/");
+  });
+});
 
-    
-        
-    
-})
-
-module.exports = router
+module.exports = router;
