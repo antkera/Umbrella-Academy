faef46cb21cb3de5d22f189ca152d3603bdc0b84
antkera <128487289+antkera@users.noreply.github.com>
2023-11-14 18:02:54 +0100

    Crud done

--- routes/admin/admin.routes.js
+++ routes/admin/admin.routes.js
@@ -5,8 +5,6 @@ const User = require("../../models/User.model");
 const moment = require("moment");
 const Enrolment = require("../../models/Enrolment.model");
 
-
-
 // GET "/admin" => renderiza la vista principal del admin
 router.get("/", (req, res, next) => {
   res.render("admin/home");
@@ -52,7 +50,7 @@ router.get("/users/:id/edit", async (req, res, next) => {
       educativeLevel,
       enrolments,
       role,
-      _id
+      _id,
     } = await User.findById(req.params.id);
 
     res.render("admin/usersCRUD", {
@@ -64,7 +62,7 @@ router.get("/users/:id/edit", async (req, res, next) => {
       educativeLevel,
       enrolments,
       role,
-      _id
+      _id,
     });
   } catch (error) {
     next(error);
@@ -74,9 +72,15 @@ router.get("/users/:id/edit", async (req, res, next) => {
 // POST "/admin/users/:id/edit" => recibe la id de un usuario para modificarlo
 
 router.post("/users/:id/edit", async (req, res, next) => {
-
-    const {firstName, lastName, phone, age, educativeLevel, role} = req.body
-  await User.findByIdAndUpdate(req.params.id, {firstName, lastName, phone, age, educativeLevel, role})
+  const { firstName, lastName, phone, age, educativeLevel, role } = req.body;
+  await User.findByIdAndUpdate(req.params.id, {
+    firstName,
+    lastName,
+    phone,
+    age,
+    educativeLevel,
+    role,
+  });
 
   res.redirect(`/admin/users/${req.params.id}/details`);
 });
@@ -94,52 +98,186 @@ router.post("/users/:id/delete", async (req, res, next) => {
 
 // GET "/admin/users/:id/details" => renderiza los detalles de un sólo usuario.
 
-router.get("/users/:id/details" , async(req, res, next) => {
+router.get("/users/:id/details", async (req, res, next) => {
   try {
-    const {firstName, lastName, phone, age, educativeLevel, role, createdAt: createdAt , updatedAt: updatedAt, enrolments, email} = await User.findById(req.params.id)
-    const fechaC =moment(createdAt).format("LLLL")
-    const fechaU =moment(updatedAt).format("LLLL")
-    res.render("admin/usersDetails", {firstName, lastName, phone, age, educativeLevel, role, fechaC, fechaU, enrolments, email})
-  
+    const {
+      firstName,
+      lastName,
+      phone,
+      age,
+      educativeLevel,
+      role,
+      createdAt: createdAt,
+      updatedAt: updatedAt,
+      enrolments,
+      email,
+    } = await User.findById(req.params.id);
+    const fechaC = moment(createdAt).format("LLLL");
+    const fechaU = moment(updatedAt).format("LLLL");
+    res.render("admin/usersDetails", {
+      firstName,
+      lastName,
+      phone,
+      age,
+      educativeLevel,
+      role,
+      fechaC,
+      fechaU,
+      enrolments,
+      email,
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 // GET "/admin/enrollment/list" => renderiza la lista de enrollments que hay en la academia.
 
-router.get("/enrollment/list",async (req, res, next) => {
+router.get("/enrollment/list", async (req, res, next) => {
   try {
-    const enrollments = await Enrolment.find().populate("userId").populate("courseId")
-    res.render("enrollment/list", {enrollments})
+    const enrollments = await Enrolment.find()
+      .populate("userId")
+      .populate("courseId");
+    res.render("enrollment/list", { enrollments });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}) 
-
+});
 
 // GET "/admin/enrollment/create" => renderiza formulario para crear una matrícula
 
-router.get("/enrollment/create" , async(req, res, next) => {
+router.get("/enrollment/create", async (req, res, next) => {
+  try {
+    const users = await User.find().select({
+      firstName: 1,
+      lastName: 1,
+      email: 1,
+    });
+    const courses = await Course.find().select({ name: 1 });
 
-const users = await User.find().select({firstName: 1, lastName: 1, email: 1})
-const courses = await Course.find().select({name: 1})
-
-
-
-  res.render("enrollment/CRUD", {users, courses})
-  
-})
+    res.render("enrollment/CRUD", { users, courses });
+  } catch (error) {
+    next(error);
+  }
+});
 
 // POST "/admin/enrollment/create" => Crea una matricula y redirige a la lista de matriculas.
 
-router.post("/enrollment/create" , async (req, res, next) => {
-  let {userId, courseId, roleInCourse, status} = req.body
-  if (!status){status = "active"}
+router.post("/enrollment/create", async (req, res, next) => {
+  try {
+    let { userId, courseId, roleInCourse, status } = req.body;
+    if (!status) {
+      status = "active";
+    }
+    const newEnrollment = await Enrolment.create({
+      userId,
+      courseId,
+      roleInCourse,
+      status,
+    });
+    await Course.findByIdAndUpdate(courseId, {
+      $push: { enrolments: newEnrollment._id },
+    });
+    await User.findByIdAndUpdate(userId, {
+      $push: { enrolments: newEnrollment._id },
+    });
+    res.redirect("/admin/enrollment/list");
+  } catch (error) {
+    next(error);
+  }
+});
 
-  console.log({userId, courseId, roleInCourse, status});
-  await Enrolment.create({userId, courseId, roleInCourse, status})
-  console.log("supueestamente creado");
-  res.redirect("/admin/enrollment/list")
+// GET "/admin/enrollment/:id/edit" => Renderiza un formulario para editar.
+
+router.get("/enrollment/:id/edit", async (req, res, next) => {
+  try {
+    const enrollment = await Enrolment.findById(req.params.id)
+      .populate("userId")
+      .populate("courseId");
+    const users = await User.find().select({
+      firstName: 1,
+      lastName: 1,
+      email: 1,
+    });
+    const courses = await Course.find().select({ name: 1 });
+
+    const enroll = {
+      enrolled: `${enrollment.userId.email}: ${enrollment.userId.lastName}, ${enrollment.userId.firstName}`,
+      enrolledId: enrollment.userId._id,
+      enrolledCourse: enrollment.courseId.name,
+      enrolledCourseId: enrollment.courseId._id,
+      role: enrollment.roleInCourse,
+      _id: enrollment._id,
+    };
+    console.log(enrollment.courseId);
+
+    res.render("enrollment/edit", { enroll, users, courses });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// POST "/admin/enrollment/:id/edit" => Actualiza un enrollment y redirige a details.
+
+router.post("/enrollment/:id/edit", async (req, res, next) => {
+  try {
+    let { userId, courseId, roleInCourse, status } = req.body;
+    if (!status) {
+      status = "active";
+    }
+    const newEnrollment = await Enrolment.findByIdAndUpdate(req.params.id, {
+      userId,
+      courseId,
+      roleInCourse,
+      status,
+    });
+    await Course.findByIdAndUpdate(newEnrollment.courseId, {
+      $pull: { enrolments: newEnrollment._id },
+    });
+    await Course.findByIdAndUpdate(courseId, {
+      $push: { enrolments: newEnrollment._id },
+    });
+    await User.findByIdAndUpdate(newEnrollment.userId, {
+      $pull: { enrolments: newEnrollment._id },
+    });
+    await User.findByIdAndUpdate(userId, {
+      $push: { enrolments: newEnrollment._id },
+    });
+    res.redirect(`/admin/enrollment/${req.params.id}/details`);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// GET "//details"
+
+router.get("/enrollment/:id/details" , async (req, res, next) => {
+
+  try {
+    const enrollment = await Enrolment.findById(req.params.id)
+    .populate("userId")
+    .populate("courseId")
+  res.render("enrollment/details", enrollment)
+  } catch (error) {
+    next(error)
+  }
+  
 })
+
+// POST "/admin/enrollment/:id/delete"
+router.post("/enrollment/:id/delete", async (req, res, next) => {
+  try {
+    const enrollmentDeleted = await Enrolment.findByIdAndDelete(req.params.id);
+    await Course.findByIdAndUpdate(enrollmentDeleted.courseId, {
+      $pull: { enrolments: enrollmentDeleted._id },
+    });
+
+    await User.findByIdAndUpdate(enrollmentDeleted.userId, {
+      $pull: { enrolments: enrollmentDeleted._id },
+    });
+
+    res.redirect("/admin/enrollment/list");
+  } catch (error) {}
+});
+
 module.exports = router;
