230920ea869c9f2b2a60f5383d4e4f7b4655ffe2
jairohrdev <j.gcarballa.dev@gmail.com>
2023-11-16 11:34:32 +0100

    modal

--- public/js/modal.js
+++
@@ -1,9 +0,0 @@
-
-const openModal = document.querySelector(".modal-open")
-const modal = document.querySelector(".modal")
-const closeModal = document.querySelector(".modal-close")
-
-openModal.addEventListener("click", (e) => {
-    e.preventDefault();
-    modal.classList.add("modal-show")
-})
