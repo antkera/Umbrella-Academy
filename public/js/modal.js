6d80e505e52a185b113dbb137b2a7f3c3c98dd8f
antkera <128487289+antkera@users.noreply.github.com>
2023-11-16 11:36:35 +0100

    modals done

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,9 +1,32 @@
 
-const openModal = document.querySelector(".modal-open")
-const modal = document.querySelector(".modal")
-const closeModal = document.querySelector(".modal-close")
+// const openModal = document.querySelector(".modal-open")
 
-openModal.addEventListener("click", (e) => {
-    e.preventDefault();
-    modal.classList.add("modal-show")
-})
+
+
+
+
+
+
+// const modal = document.querySelector(".modal")
+// const closeModal = document.querySelector(".modal-close")
+
+
+// openModal.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     console.log(openModal.values);
+//     // modal.classList.add("modal-show")
+// })
+
+
+
+// 		// var modal1 = new bootstrap.Modal(document.getElementById('gfg')); 
+// 		// function toggleModal1() { 
+			
+// 		// 	// Toggle Modal 
+// 		// 	modal1.toggle(); 
+			
+// 		// 	// Toggle the modal again after 3 seconds 
+// 		// 	setTimeout(() => { 
+// 		// 		modal1.toggle(); 
+// 		// 	}, 3000) 
+// 		// } 
