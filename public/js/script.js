27a9a44b342182c5ace32dd5788484bc0c3a7d97
jairohrdev <j.gcarballa.dev@gmail.com>
2023-11-16 09:01:45 +0100

    agragados estilos/mapa

--- public/js/script.js
+++ public/js/script.js
@@ -2,3 +2,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Umbrella-academy JS imported successfully!");
 });
+
+let map = L.map("map").setView([41.3977421, 2.1902493], 15);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "© OpenStreetMap contributors",
+}).addTo(map);
+L.marker([41.3977421, 2.1902493])
+  .addTo(map)
+  .bindPopup("¡Hola! Aquí estamos.")
+  .openPopup();
